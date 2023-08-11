32a4b52cd8e0dfa1a8fd182a36dedb58d16124ac
dmasequesmay <133715991+dmasequesmay@users.noreply.github.com>
2023-08-11 14:04:36 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// Place charCnt prototype (declaration) here
+int charCnt(string filename, char ch);
+
+
+int main() {
+   string filename;
+   char ch;
+   int chCnt = 0;
+   
+   cout << "Enter the name of the input file: ";
+   cin >> filename;
+   cout << endl;
+   cout << "Enter a character: ";
+   cin.ignore(); // ignores newline left in stream after previous input statement
+   cin.get(ch);
+   cout << endl;
+
+   chCnt = charCnt(filename, ch);   
+   cout << "# of " << ch << "'s: " << chCnt << endl;
+   
+   return 0;
+}
+
+// Place charCnt implementation here
+
+int charCnt(string filename, char ch) {
+   ifstream inFS;
+   inFS.open(filename);
+   char myChar;
+   int count = 0;
+
+   if (!inFS.is_open()) {
+      cout << "Error opening " << filename << endl;
+      exit(1);
+   }
+
+   while (inFS.get(myChar)) {
+      if (myChar == ch) {
+         ++count;
+      }
+   }
+
+return count;
+
+}
